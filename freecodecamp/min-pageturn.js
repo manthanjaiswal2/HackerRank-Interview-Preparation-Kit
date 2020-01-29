c0275fd822cd8f269c61b4bfa521e6dca75469fb
manthanjaiswal2 <35054059+manthanjaiswal2@users.noreply.github.com>
2020-01-30 00:46:03 +0530

    changes done

--- freecodecamp/min-pageturn.js
+++
@@ -1,8 +0,0 @@
-// Here n is the number of pages in the books and p is the desired page. This will give the minimum number of turn to get the desired page.
-
-function pageCount(n, p) {
-    
-    return Math.min(Math.floor(p / 2), Math.floor(n / 2) - Math.floor(p / 2));
-}
-
-pageCount(5,4);
--- freecodecamp/spinal_tap.js
+++
@@ -1,5 +0,0 @@
-function spinalTap(str) {
-	return str.replace(/[\s_]/g,'-').replace(/([a-z])([A-Z])/g,'$1-$2').toLowerCase();
-}
-spinalTap("ThisIsMe");
-
